669002db3ae8b10ff7667921dc782dc5ff5915e0
VincentCastig <vincent.castig@gmail.com>
2018-04-20 11:48:45 -0500

    Remove Images and Style Header

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,7 +21,7 @@ class Home extends Component {
             <div className="home-wine-container">
                 <div className="content-row">
                     <Col xs={6} md={6} className="slider">
-                        <img className="home-main-image slide1" src={(require('../Img/backApt.JPG'))} alt="Apartment unit 7 and 6"/>
+                        <img className="home-main-image slide1" src={(require('../Img/DSC_0569.JPG'))} alt="Apartment unit 7 and 6"/>
                         <img className="home-main-image slide2" src={(("https://lh3.google.com/u/0/d/1K7rvx9tFzD8A9aQqIs-R8QkErmCTl6EY=w1830-h915-iv1"))} alt="Apartment Kitchen"/>
                         <img className="home-main-image slide3" src={(("https://lh3.google.com/u/0/d/1KHCDvT-3Gf-TbOQymgVx1jPksC7Kh9y5=w1830-h915-iv1"))} alt="Apartment Bedroom and Bathroom"/>
                     </Col>
@@ -38,7 +38,7 @@ class Home extends Component {
                     <div xs={12} md={12} className="mapHeader">
     
                         {/* <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type ... Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum</p> */}
-                        <h4 className="red-header">Apartments are located at 611 Sarrazin Ave, Pascagoula, MS 39567</h4>
+                        <h4 className="red-header">Apartments are located at 611 Sarazzin Ave, Pascagoula, MS 39567</h4>
                         {/* <p>We are conveniantly located on the corner of Market St and Covenant St. Come by to take a look at our large selection of wine, liquor, and liqueurs.</p> */}
                         
                     </div>
