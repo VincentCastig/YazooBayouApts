fca18874d5961113bfb86c4ca46c77b9f27e49fd
VincentCastig <vincent.castig@gmail.com>
2024-09-13 16:02:56 -0500

    restyle header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import '../styles/header.scss';
+import logoImage from '../Img/logo/yb-logo.png'
 
 
 const Header = () => {
     return (
-        <div>
+        <header>
             <div className="jumbotron">
                 <div className="inner">
+                    {/* <div className='logo-image left'>
+                        <img alt='Yazoo Bayou Apartment Logo - Left' src={logoImage} />
+                    </div> */}
                     <h1>Yazoo Bayou Apartments</h1>
-                    <div className='logo-image'></div>
+                    <div className='logo-image right'>
+                        <img alt='Yazoo Bayou Apartment Logo' src={logoImage} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </header>
 
     )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,35 +18,32 @@ const Home = () => {
             </section>
 
             <main className="container-home">
-                <div className="home-paragraph-wrapper">
-                    {/* <div className='welcome-text'>
-                        Welcome
-                    </div> */}
+                <article className="home-paragraph-wrapper">
                     <h2>Yazoo Bayou</h2>
                     <p>Welcome to Yazoo Bayou Apartments, where comfort meets convenience in the heart of Pascagoula, MS. Our apartments offer a serene atmosphere, making it the perfect place to call home. Enjoy peaceful living with scenic bayou views and easy access to local attractions in Pascagoula.</p>
-                </div>
-                <div className="home-wine-container">
+                </article>
+                <section className="home-wine-container">
                     <div className="content-row">
                         <Col xs={12} md={6} className="slider">
-                            <img className="home-main-image slide1" src={apartment1Image} alt="Apartment unit 7 and 6" />
-                            <img className="home-main-image slide2" src={kitchenImage} alt="Apartment Kitchen" />
-                            <img className="home-main-image slide3" src={bedroomImage} alt="Apartment Bedroom and Bathroom" />
+                            <img className="home-main-image slide1" src={apartment1Image} alt="Apartment units 6 and 7" />
+                            <img className="home-main-image slide2" src={kitchenImage} alt="Kitchen in Yazoo Bayou Apartments" />
+                            <img className="home-main-image slide3" src={bedroomImage} alt="Bedroom in Yazoo Bayou Apartments" />
                         </Col>
                         <Col xs={12} md={6}>
                             <h3 className="red-header">8 Unit Apartment Complex</h3>
                             <h3 className="red-header">Rates: $675 and $725 Per Month</h3>
-                            <h3 className="red-header">On Site Parking</h3>
+                            <h3 className="red-header">On-site Parking</h3>
                             <h3 className="red-header">Water Included</h3>
                             <h3 className="red-header">For Inquiries, Call Liquors Unlimited at 228.762.3874</h3>
                         </Col>
                     </div>
-                    <div id="content-row2">
+                    <section id="content-row2">
                         <div className="mapHeader">
-                            <h4 className="red-header">Apartments are located at 611 Sarazzin Ave, Pascagoula, MS 39567</h4>
+                            <h4 className="red-header">Located at 611 Sarazzin Ave, Pascagoula, MS 39567</h4>
                         </div>
                         <Map />
-                    </div>
-                </div>
+                    </section>
+                </section>
             </main>
         </div>
     );
